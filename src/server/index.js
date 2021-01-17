ec7b2ac2dc0cc6b0b9d10825e6940cb8fc655083
Cristian Escudero <cristian.escudero@mercadolibre.com>
2021-01-17 12:39:29 -0300

    Add working SSR code

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
 const Vue = require('vue')
+
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync(path.join(__dirname, '../public', 'index.template.html'), 'utf-8'),
+})
 
 server.get('*', async (req, res) => {
   const app = new Vue({
@@ -9,20 +15,21 @@ server.get('*', async (req, res) => {
         url: req.url,
       }
     },
-    template: '<div>The visited url is {{ url }}</div>',
+    template: '<div>The visited URL is: {{ url }}</div>',
   })
+  const context = {
+    title: 'Home Page',
+    meta: `
+      <meta charset="UTF-8">
+      <meta name="description" content="The example description">
+    `,
+  }
 
   try {
-    const html = await renderer.renderToString(app)
-
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    const html = await renderer.renderToString(app, context)
+    res.end(html)
   } catch (err) {
+    console.error(err)
     res.status(500).end('Internal Server Error')
   }
 })
